dd48957ac90e4e94febb032a41b33db13ccae808
Vanshika2703 <reddyvs@rose-hulman.edu>
2020-10-19 00:45:28 -0400

    displays lists and modal works, no javascript

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,29 +1,57 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
 
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+//From: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+rhit.ListPageController = class {
 	constructor() {
+		// document.querySelector("#submitAddQuote").onclick = (event) =>{}
+		document.querySelector("#back").addEventListener("click",(event)=>{
+			const list = newList(1,document.querySelector("#inputTitle").value,document.querySelector("#inputItem").value);
+			const title = document.querySelector("#inputTitle").value;
+			const item = document.querySelector("#inputItem").value;
+			
+		});
 
+		$("#newListModal").on("show.bs.modal", (event) => {
+			document.querySelector("#inputTitle").value = "";
+			document.querySelector("#inputItem").value = "";
+		});
+	}
+	updateList() {
+		//make a new quote list container
+		const newList = htmlToElement('<div id="listContainer"></div>');
+		//fill them with quote cards
+			const newCard = this._createCard(list.title, list.item);
+			newList.appendChild(newCard);
+		//remove the old one
+		const oldList = document.querySelector("#listContainer");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+		// put in the new one
+		oldList.parentElement.appendChild(newList);
 	}
 
-	methodName() {
+	_createCard(title, item){
+		return htmlToElement(`<div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${item}</h6>
+        </div>
+      </div>`);
+	}
+   }
 
+rhit.List = class {
+	constructor(id, title,items) {
+	  this.id = id;
+	  this.title = title;
+	  this.items = items;  
 	}
 }
 
