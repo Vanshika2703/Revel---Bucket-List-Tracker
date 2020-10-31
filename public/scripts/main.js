ff9fa6b6c5efb847575c3f1bc918173157a27680
Vanshika2703 <reddyvs@rose-hulman.edu>
2020-10-31 15:53:57 -0400

    commented manager

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,6 +11,13 @@ revel.FB_KEY_PICTURE = "Picture";
 revel.FB_KEY_ISCHECKED = "isChecked";
 revel.FB_KEY_JOURNAL = "journalEntry";
 revel.fbBucketListManager = null;
+revel.pages = {
+	"friendsPage" : "friends.html",
+	"ExpandedList" : "expandedList.html",
+	"Index" : "index.html",
+	"Profile" : "profile.html",
+	"Timeline" : "timeline.html",
+}
 
 
 //From: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
@@ -19,6 +26,8 @@ function htmlToElement(html) {
     template.innerHTML = html;
     return template.content.firstChild;
 }
+
+
 
 revel.ListPageController = class {
 	constructor() {
@@ -53,8 +62,9 @@ revel.ListPageController = class {
 		return htmlToElement(`<div class="card">
         <div class="card-body">
           <h5 class="card-title">${title}</h5>
-          ${this._createItems(items)}
-        </div>
+          <div class="checkbox col">
+		  	${this._createItems(items)}
+          </div>
       </div>`);
 	}
 	_createItems(items){
@@ -63,10 +73,10 @@ revel.ListPageController = class {
 			itemHtml += `<div class="row checkbox"> <label> <input type="checkbox" class="item"> <h5>${item}</h5> </label> </div>`
 		});
 		return itemHtml;
-	}       
+	}         
 }
 
-revel.detailPageController = class {
+revel.expandedListController = class {
 	constructor() {
 		// document.querySelector("#submitAddQuote").onclick = (event) =>{}
 		document.querySelector("#back").addEventListener("click",(event)=>{
@@ -161,52 +171,53 @@ revel.checkForRedirects = function() {
 	}
 };
 
-revel.FbBucketListManager = class {
-	constructor() {
-	  this._documentSnapshots = [];
-	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_LISTS);
-	  this._unsubscribe = null;
-	}
-	add(quote, movie) {  
-		console.log(`${quote}, ${movie}`);	
-		// Add a new document with a generated id.
-		this._ref.add({
-			[rhit.FB_KEY_QUOTE] : quote,
-			[rhit.FB_KEY_MOVIE] : movie,
-			[rhit.FB_KEY_LAST_TOUCHED] : firebase.firestore.Timestamp.now(),
-		})
-		.then(function(docRef) {
-			console.log("Document written with ID: ", docRef.id);
-		})
-		.catch(function(error) {
-			console.error("Error adding document: ", error);
-		});
-	}
-	beginListening(changeListener) {    
-		this._unsubscribe = this._ref
-		.orderBy(rhit.FB_KEY_LAST_TOUCHED, "desc")
-		.limit(50)
-		.onSnapshot((querySnapshot) => {
-				console.log("MovieQuote update!");
-				this._documentSnapshots = querySnapshot.docs;
-				changeListener();
-			});
+// revel.FbBucketListManager = class {
+// 	constructor() {
+// 	  this._documentSnapshots = [];
+// 	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_LISTS);
+// 	  this._itemref = this._ref.FB_COLLECTION_ITEMS
+// 	  this._unsubscribe = null;
+// 	}
+// 	addList(title,items) {  
+// 		console.log(`${title}, ${items}`);	
+// 		// Add a new document with a generated id.
+// 		this._ref.add({
+// 			[rhit.FB_KEY_QUOTE] : title,
+			
+// 			[rhit.FB_KEY_LAST_TOUCHED] : firebase.firestore.Timestamp.now(),
+// 		})
+// 		.then(function(docRef) {
+// 			console.log("Document written with ID: ", docRef.id);
+// 		})
+// 		.catch(function(error) {
+// 			console.error("Error adding document: ", error);
+// 		});
+// 	}
+// 	beginListening(changeListener) {    
+// 		this._unsubscribe = this._ref
+// 		.orderBy(rhit.FB_KEY_LAST_TOUCHED, "desc")
+// 		.limit(50)
+// 		.onSnapshot((querySnapshot) => {
+// 				console.log("MovieQuote update!");
+// 				this._documentSnapshots = querySnapshot.docs;
+// 				changeListener();
+// 			});
 		
-	}
-	stopListening() {    
-		this._unsubscribe();
-	}
-	get length() {    
-		return this._documentSnapshots.length;
-	}
-	getMovieQuoteAtIndex(index) {
-		const docSnapshot = this._documentSnapshots[index];
-		const mq = new rhit.MovieQuote(docSnapshot.id,
-			docSnapshot.get(rhit.FB_KEY_QUOTE),
-			docSnapshot.get(rhit.FB_KEY_MOVIE));
-		return mq;
-	}
-   }
+// 	}
+// 	stopListening() {    
+// 		this._unsubscribe();
+// 	}
+// 	get length() {    
+// 		return this._documentSnapshots.length;
+// 	}
+// 	getMovieQuoteAtIndex(index) {
+// 		const docSnapshot = this._documentSnapshots[index];
+// 		const mq = new rhit.MovieQuote(docSnapshot.id,
+// 			docSnapshot.get(rhit.FB_KEY_QUOTE),
+// 			docSnapshot.get(rhit.FB_KEY_MOVIE));
+// 		return mq;
+// 	}
+//    }
 
 revel.main = function () {
 	console.log("Ready");
